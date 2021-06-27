1e05b49300b0fd7c77a5fbf18e2fe9dfeba2cbfa
juro106 <hiyuman@gmail.com>
2021-06-27 23:45:58 +0900

    no javascript

--- builder.go
+++ builder.go
@@ -8,7 +8,7 @@ import (
 	// "math"
 	// "net/http"
 	// "net/url"
-    "reflect"
+    // "reflect"
 	"io"
 	"os"
 	// "os/exec"
@@ -20,7 +20,7 @@ import (
 	"strings"
 	// "time"
     "html/template"
-    "encoding/json"
+    // "encoding/json"
 
 	// "github.com/flosch/pongo2"
 	// "github.com/howeyc/fsnotify"
@@ -28,6 +28,7 @@ import (
     "github.com/yuin/goldmark"
     // "github.com/yuin/goldmark/extension"
     // "github.com/yuin/goldmark/parser"
+    // "github.com/yuin/goldmark-highlighting"
     // "github.com/yuin/goldmark/renderer/html"
 	"gopkg.in/yaml.v2"
 )
@@ -37,21 +38,35 @@ func checkFatal(err error) {
         log.Fatal(err)
     }
 }
+type Item struct {
+    Title string
+    Slug  string
+    Description string
+}
+type PageB2Tag map[string][]Item
 
 type Meta struct {
     Title         string                     `yaml:"title"`        // meta
     Description   string                     `yaml:"description"`  //
     Tags          []string                   `yaml:"tags"`         //
+    DateP         string
     DatePublished string                     `yaml:"datePublished"`//
+    DateM         string
     DateModified  string                     `yaml:"dateModified"` //
     Draft         bool                       `yaml:"draft"`        //
     Home          bool                       `yaml:"home"`        //
     Fixed         bool                       `yaml:"fixed"`         //
+    Code          bool                       `yaml:"code"`         //
     Option        []string                   `yaml:"option"`         //
     Layout        string                     `yaml:"layout"`       //
     Slug          string                     `yaml:"slug"`         //
     Permalink     string                     `yaml:"permalink"`         //
     Body          template.HTML              `yaml:"body"`         //
+    Dist          string
+    PageTag       PageB2Tag
+    B2Page        []Item
+    Plist         []Meta
+    Site          *Config
 }
 
 type Config struct {
@@ -72,14 +87,21 @@ type Config struct {
     MarkdownExt string                       `yaml:"markdown_ext"`
 }
 
+// config
 var cfg Config
 // 全ページのデータリスト
 var metalist = make([]Meta, 0)
 // 投稿ページのリスト
 var postlist = make([]Meta, 0)
+// 固定ページのリスト
+var pagelist = make([]Meta, 0)
+// タグページのリスト
+var tagplist = make([]Meta, 0)
 // tag リスト
 var taglist []string
-
+// page-belong-to-tag リスト
+var pageB2taglist PageB2Tag = PageB2Tag{}
+// map[string][]string = map[string][]string{}
 
 func time2int (args interface{}) int {
     dateTime := args.(string)
@@ -132,24 +154,22 @@ func urlJoin(l, r string) string {
     return l + r + "/"
 }
 
-func (cfg *Config) convertFile(dirName, tpl string) {
-    // テンプレートの読み込み
-    t := template.Must(template.ParseFiles(tpl))
-
+// ページ作成の準備、タグページや関連リンク作成用の下準備
+func (cfg *Config) collectData(dirName string) {
     // ディレクトリのファイル一覧を得る
     files, err := os.ReadDir(dirName)
     if err != nil {
         log.Fatal(err)
     }
-
     // dirName内のファイルをループ
     for _, file := range files {
-        // markdownファイルからHTMLファイル作成
-        var fpath = file.Name()
-        fmt.Printf("fpath: %#v\n", fpath)
-        srcFile := filepath.Join(dirName, fpath)
-        // fmt.Println(srcFile)
+        // データ登録用の構造体を用意
+        var meta Meta
+        // サイトの基本情報を metadata に加える
+        meta.Site = cfg
         // ファイルの中身を読み取る
+        var fpath = file.Name()
+        srcFile := filepath.Join(dirName, fpath)
         b, e := os.ReadFile(srcFile)
         if e != nil {
             log.Fatal(e)
@@ -172,22 +192,15 @@ func (cfg *Config) convertFile(dirName, tpl string) {
         if err = goldmark.Convert([]byte(content), &buf); err != nil {
             panic(err)
         }
-        new_html := buf.Bytes()
-
+        // markdown から html へ変換したものを Body へ登録
+        meta.Body = template.HTML(buf.Bytes())
         // frontmatter を取得
-        var meta Meta
         err = yaml.Unmarshal([]byte(b), &meta)
         if err != nil {
             log.Fatalf("error: %v", err)
         }
-        // コンテンツ部分を 構造体へ追加 安全性に関して考える必要がある
-        // fmt.Println(reflect.TypeOf(meta.Body))
-
-        // 加工してから加える必要があるデータ
+        // slug を登録
         slug := filepath.Base(fpath[:len(fpath)-len(filepath.Ext(fpath))])
-        // fmt.Printf("file.Name(): %#v\n", file.Name())
-        // fmt.Printf("fpath: %#v\n", fpath)
-        // fmt.Printf("slug: %#v\n", slug)
         if slug == "index" {
             meta.Slug = "/"
             meta.Permalink = cfg.Baseurl
@@ -195,67 +208,98 @@ func (cfg *Config) convertFile(dirName, tpl string) {
             meta.Slug = slug
             meta.Permalink = urlJoin(cfg.Baseurl, slug)
         }
-
-        // fmt.Printf("Bseurl: %#v\n", cfg.Baseurl)
-        fmt.Printf("permalink: %#v\n", meta.Permalink)
-
-        fmt.Println(reflect.TypeOf(meta))
-        meta.Body = template.HTML(new_html)
-        // fmt.Printf("meta: %#v\n", meta)
-
-        new_buf := new(bytes.Buffer)
+        // 日付変換
+        if len(meta.DatePublished) > 0 {
+            meta.DateP = meta.DatePublished[:10]
+        }
+        if len(meta.DateModified) > 0 {
+            meta.DateM = meta.DateModified[:10]
+        }
+        // デプロイ先の登録
         dst := "./_site"
         if fpath == "index.md" && dirName == "./_pages" {
-            dst = filepath.Join(dst, "index.html")
+            meta.Dist = filepath.Join(dst, "index.html")
         } else {
             dstDir := filepath.Join(dst, slug)
             err = os.MkdirAll(dstDir, 0755)
-            dst = filepath.Join(dst, slug, "index.html")
-        }
-
-        if err = t.Execute(new_buf, meta); err != nil {
-            log.Println("create file", err)
+            meta.Dist = filepath.Join(dst, slug, "index.html")
         }
         // tag が1つ以上あったら taglist に追加
         if len(meta.Tags) > 0 {
+            // page-tag-list
             taglist = append(taglist, meta.Tags...)
+            // tag-page-list
+            for _, tag := range meta.Tags {
+                var item Item
+                item.Title = meta.Title
+                item.Slug = meta.Slug
+                item.Description = meta.Description
+                pageB2taglist[tag] = append(pageB2taglist[tag], item)
+            }
         }
-        // 投稿ページの場合は、リストへデータ追加
-        if dirName == "./_posts" {
+        // リストへデータ追加
+        if dirName == "./_pages" {
+            pagelist = append(pagelist, meta)
+        } else if dirName == "./_posts" {
             postlist = append(postlist, meta)
+        } else if dirName == "./_tags" {
+            tagplist = append(tagplist, meta)
         }
+        // 全ページデータへ追加
         metalist = append(metalist, meta)
+    }
+}
 
-        // fmt.Println(new_buf)
-        os.WriteFile(dst, new_buf.Bytes(), 0644)
-        fmt.Printf("WriteFile完了\n==================\n")
-
+// ファイルを作成して書き込み
+func (cfg *Config) convertFile(tpl, ptype string) {
+    var list []Meta
+    if ptype == "page" {
+        list = pagelist
+    } else if ptype == "post" {
+        list = postlist
+    } else if ptype == "tag" {
+        list = tagplist
+    }
+    // テンプレートの読み込み
+    t := template.Must(template.ParseFiles(tpl, "./_layouts/head.html", "./_layouts/footer.html"))
+    for _, meta := range list {
+        if ptype == "page" && meta.Slug == "/" {
+            meta.Plist = postlist
+        }
+        if ptype == "post" {
+            var postB2tag PageB2Tag = PageB2Tag{}
+            for _, tag := range meta.Tags {
+                postB2tag[tag] = pageB2taglist[tag]
+            }
+            meta.PageTag = postB2tag
+        }
+        if ptype == "tag" {
+            meta.B2Page = pageB2taglist[meta.Slug]
+        }
+        // テンプレートへ反映させて書き込み
+        new_buf := new(bytes.Buffer)
+        if err := t.Execute(new_buf, meta); err != nil {
+            log.Println("create file", err)
+        }
+        os.WriteFile(meta.Dist, new_buf.Bytes(), 0644)
+        fmt.Printf("<<===== ConvertFile %s WriteFile完了 ======>>>\n", meta.Dist)
     }
 }
 
 func main() {
-    fmt.Printf("cfg: %+v\n", cfg)
+    // fmt.Printf("cfg: %+v\n", cfg)
     // config を読み込み
     buf, err := os.ReadFile("config.yaml")
     if err != nil {
         log.Fatal(err)
     }
-    // fmt.Printf("buf: %+v\n", string(buf))
-    // var cfg Config
     err = yaml.Unmarshal(buf, &cfg)
     if err != nil {
         log.Fatal(err)
     }
-    fmt.Printf("cfg: %+v\n", cfg)
-
-    // テンプレートデータのpath
-    layout := "./_layouts/default2.html"
-
-
-    // pages ページ生成
-    cfg.convertFile("./_pages", layout)
-    // posts ページ生成
-    cfg.convertFile("./_posts", layout)
+    // データ収集 
+    cfg.collectData("./_pages")
+    cfg.collectData("./_posts")
 
     // tag ページ生成
     // ① taglistの重複を削除する
@@ -269,11 +313,8 @@ func main() {
             tagList = append(tagList, elem)
         }
     }
-
-    // 比較確認用
-    fmt.Printf("mapの中身: %#v\n", taglistM)
-    fmt.Printf("重複削除後: %#v\n", tagList)
-
+    // fmt.Printf("mapの中身: %#v\n", taglistM)
+    // fmt.Printf("重複削除後: %#v\n", tagList)
     // ② tag ベースになる .md ファイルを生成（既にあるものはスルー）※postsを生成してからじゃないと実行できない
     dirName := "./_tags/"
     for _, tag := range tagList {
@@ -284,9 +325,13 @@ func main() {
             copyFile("./_layouts/tag.md", srcFile)
         }
     }
-
+    // pages ページ生成
+    cfg.convertFile("./_layouts/default.html", "page")
+    // posts ページ生成
+    cfg.convertFile("./_layouts/post.html", "post")
     // tag ページ生成
-    cfg.convertFile("./_tags", layout)
+    cfg.collectData("./_tags")
+    cfg.convertFile("./_layouts/tag.html", "tag")
 
     // fmt.Printf("type of taglist: %#v\n", reflect.TypeOf(taglist))
     // fmt.Printf("taglist: %#v\n", taglist)
@@ -295,29 +340,47 @@ func main() {
     cssDir := filepath.Join(cfg.Destination, "css")
     err = os.MkdirAll(jsDir, 0755)
     err = os.MkdirAll(cssDir, 0755)
-    copyFile("./_assets/js/main.js", "./_site/js/main.js")
-    copyFile("./_assets/js/top-page.js", "./_site/js/top-page.js")
+    // copyFile("./_assets/js/main.js", "./_site/js/main.js")
+    // copyFile("./_assets/js/top-page.js", "./_site/js/top-page.js")
+    copyFile("./_assets/js/prism.js", "./_site/js/prism.js")
     copyFile("./_assets/css/style.css", "./_site/css/style.css")
 
-    f, err := os.OpenFile("./_site/js/main.js", os.O_APPEND | os.O_WRONLY, 0600)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer f.Close()
-    fmt.Println(tagList);
-    jsTagList := strings.Join(tagList, "\",\"")
-    jsTagList = "var tags = [\"" + jsTagList + "\"];"
-    fmt.Fprintln(f, jsTagList);
+    // f, err := os.OpenFile("./_site/js/main.js", os.O_APPEND | os.O_WRONLY, 0600)
+    // if err != nil {
+    //     log.Fatal(err)
+    // }
+    // defer f.Close()
+    // fmt.Printf("tagList: %s\n", tagList)
+    // jsTagList := strings.Join(tagList, "\",\"")
+    // jsTagList = "var tags = [\"" + jsTagList + "\"];"
+    // fmt.Fprintln(f, jsTagList);
 
     // // 日付降順に並べる トップページの一覧用
     sort.Slice(postlist, func(i, j int) bool { return time2int(postlist[i].DateModified) > time2int(postlist[j].DateModified) })
-    // fmt.Println(metalist)
+    // for _, elem := range metalist {
+    //     fmt.Printf("%v\n", elem.Dist)
+    // }
 
-    // json化 javascript で扱うときのため
-    data_json, _ := json.Marshal(&metalist)
-    post_json, _ := json.Marshal(&postlist)
-    // fmt.Printf("[page-data.json]: %v\n", string(sample_json))
-    os.WriteFile("./_site/data/page-data.json", data_json, 0644)
-    os.WriteFile("./_site/data/post-data.json", post_json, 0644)
+    // // json化 javascript で扱うときのため
+    // data_json, _ := json.Marshal(&metalist)
+    // post_json, _ := json.Marshal(&postlist)
+    // // fmt.Printf("[page-data.json]: %v\n", string(sample_json))
+    // os.WriteFile("./_site/data/page-data.json", data_json, 0644)
+    // os.WriteFile("./_site/data/post-data.json", post_json, 0644)
+
+    sitemap := make([]string, 0)
+    plist := make([]string, 0)
+    for _, post := range metalist {
+        p := "<url><loc>" + post.Permalink + "</loc></url>"
+        plist = append(plist, p)
+    }
+    // fmt.Printf("plist: %s\n", plist)
+    sitemap = append(sitemap, "<urlset xmlns='http://www.sitemaps.org/schemas/sitemap/0.9'>")
+    // strings.Join(plist, "")
+    sitemap = append(sitemap, plist...)
+    sitemap = append(sitemap, "</urlset>")
+    sitemapStr := strings.Join(sitemap, "\n")
+    // fmt.Printf("sitemap: %s\n", sitemapStr)
+    os.WriteFile("./_site/sitemap.xml", []byte(sitemapStr), 0644)
 }
 
